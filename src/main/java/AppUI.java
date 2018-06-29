83a6ef8ded673adc5ae0e44e2d7b823a6638d298
Cisco <fjdiaz2k11@hotmail.com>
2018-06-28 20:10:55 -0500

    Fix VM issue on start config file

--- src/main/java/AppUI.java
+++ src/main/java/AppUI.java
@@ -7,27 +7,42 @@ import java.io.IOException;
 
 public class AppUI extends JPanel implements ActionListener {
     private JFrame window1, window2;
-    private JPanel panel, panel2;
-    private JButton newButton, recipeButton, doneButton;
+    private JPanel panel, panel2, welcome;
+    private JButton newButton, recipeButton, doneButton, sampleButton;
     private JLabel title, recipeName;
     private JTextArea text;
 
     public void CookBook() {
         window1 = new JFrame("Cook Book");
         window1.pack();
+        panel = new JPanel(new GridBagLayout());
+        panel.setLayout(new GridBagLayout());
 
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout());
 
-        title = new JLabel("Welcome to Cook Book!");
+//        GridBagConstraints c = new GridBagConstraints();
+//        c.fill = GridBagConstraints.FIRST_LINE_START;
+//        c.gridx = 0;
+//        c.gridy = 0;
         newButton = new JButton("New Recipe");
 
         newButton.addActionListener(this);
 
-        panel.add(title, "North");
-        panel.add(newButton, "South");
+
+//        panel.add(newButton, c);
+
+//        welcome = new JPanel(new GridBagLayout());
+//        GridBagConstraints d = new GridBagConstraints();
+//        d.fill = GridBagConstraints.PAGE_END;
+//        d.gridx = 5;
+//        d.gridy = 5;
+        sampleButton = new JButton("TEST");
+//        welcome.add(sampleButton, d);
+        title = new JLabel("Welcome to Cook Book!");
+        panel.add(title);
+
 
         window1.getContentPane().add(panel, "North");
+       // window1.getContentPane().add(welcome, "South");
         window1.setSize(500, 500);
         window1.setVisible(true);
         window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
